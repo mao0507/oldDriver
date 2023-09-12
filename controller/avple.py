7741eb8569bde4d846dc7e03e13bf71e7610a912
Mao <37288931+mao0507@users.noreply.github.com>
2023-09-12 22:44:12 +0800

    feat: add avple controller

---
+++ controller/avple.py
@@ -0,0 +1,26 @@
+import re
+import bs4
+import requests
+from utils.config import headers
+from utils.common import createdFolder
+from utils.downloadModule import Download_m3u8
+
+def start(url):
+  #print(url)
+  title = getTitle(url)
+  folderPath = createdFolder(title)
+  Download_m3u8(url,title,folderPath)
+  
+def getTitle(url):
+   # 請求網頁資料
+    req = requests.get(url, headers=headers)
+    # 調整網頁格式，防止亂碼
+    req.encoding = 'utf-8'
+    # 取得網頁節點
+    html = bs4.BeautifulSoup(req.text, 'html.parser')
+    # 取得網頁標籤 並做處理
+    title = html.title.getText().split('|')[0].replace(' ', '').replace('）', ')').replace('（', '(').replace('&', '_').replace('/', '').replace('?', '_')
+       
+    
+    
+    return title
